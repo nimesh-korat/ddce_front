79b42d05e990652735ff37c7b56cb56ac18fe58d
nimesh-korat <nimeshkorat611@gmail.com>
2024-11-27 17:43:38 +0530

    first commit

--- src/modules/home_three/components/Widget.jsx
+++ src/modules/home_three/components/Widget.jsx
@@ -6,36 +6,44 @@ function HomeWidgets({ bgColor, img, title, count, isLocked }) {
           <div className="card-body position-relative">
             {isLocked && (
               <div className="lock-overlay active">
-                <i className="ph-thin ph-lock" />{" "}
+                <i className="ph-fill ph-lock" />{" "}
               </div>
             )}
-            {isLocked ? (<><div className="content"><div className="flex-between gap-8 mb-24">
-              <span
-                className={`flex-shrink-0 w-48 h-48 flex-center rounded-circle bg-main-600 text-white text-2xl ${bgColor}`}
-              >
-                <i className={img} />
-              </span>
-              {/* <div
+            {isLocked ? (
+              <>
+                <div className="content">
+                  <div className="flex-between gap-8 mb-24">
+                    <span
+                      className={`flex-shrink-0 w-48 h-48 flex-center rounded-circle bg-main-600 text-white text-2xl ${bgColor}`}
+                    >
+                      <i className={img} />
+                    </span>
+                    {/* <div
               id="complete-course"
               className="remove-tooltip-title rounded-tooltip-value"
             /> */}
-            </div>
-            <h4 className="mb-2">{`${count}+`}</h4>
-            <span className="text-gray-300">{title}</span></div></>): (<><div className="flex-between gap-8 mb-24">
-              <span
-                className={`flex-shrink-0 w-48 h-48 flex-center rounded-circle bg-main-600 text-white text-2xl ${bgColor}`}
-              >
-                <i className={img} />
-              </span>
-              {/* <div
+                  </div>
+                  <h4 className="mb-2">{`${count}+`}</h4>
+                  <span className="text-gray-300">{title}</span>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex-between gap-8 mb-24">
+                  <span
+                    className={`flex-shrink-0 w-48 h-48 flex-center rounded-circle bg-main-600 text-white text-2xl ${bgColor}`}
+                  >
+                    <i className={img} />
+                  </span>
+                  {/* <div
               id="complete-course"
               className="remove-tooltip-title rounded-tooltip-value"
             /> */}
-            </div>
-            <h4 className="mb-2">{`${count}+`}</h4>
-            <span className="text-gray-300">{title}</span></>)}
-            
-            
+                </div>
+                <h4 className="mb-2">{`${count}+`}</h4>
+                <span className="text-gray-300">{title}</span>
+              </>
+            )}
           </div>
         </div>
       </div>
