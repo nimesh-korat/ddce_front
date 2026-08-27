e9a06d7f4fe41f1a34a94ab30a00d9eed6ed64f7
Nimesh Korat <nimeshkorat611@gmail.com>
2026-08-27 11:09:43 +0530

    solution page

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ import AssignBatchToTest from "./admin/AssignBatchToTest";
 import AddSession from "./admin/AddSessions";
 import ShowSession from "./admin/ShowSession";
 import AssignSessionToBatch from "./admin/AssignSessionToBatch";
-// import Solutions from "./modules/solutions";
+import Solutions from "./modules/solutions";
 import ViewGivenExamData from "./admin/ViewGivenExamData";
 import StudentWiseExamData from "./admin/StudentWiseExamData";
 import CollgePredication from "./modules/college_predication";
@@ -165,14 +165,16 @@ function App() {
               </ProtectedRoute>
             }
           />
-          {/* <Route
+          <Route
             path="/solutions"
             element={
               <ProtectedRoute>
-                <Solutions />
+                <AccessGuard feature="solutions">
+                  <Solutions />
+                </AccessGuard>
               </ProtectedRoute>
             }
-          /> */}
+          />
           {/* <Route path="/mentors" element={<Mentor />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/messages" element={<Messages />} />
